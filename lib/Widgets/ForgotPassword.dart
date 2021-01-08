96b7d95afad2d5efef2c1f3d26f858f957f5f830
Anu Baby <anubbey@gmail.com>
2021-01-08 21:08:37 +0530

    forgot password page ready

--- lib/Widgets/ForgotPassword.dart
+++ lib/Widgets/ForgotPassword.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+//import 'package:patient_portal/main.dart';
 
 class ForgotPassword extends StatefulWidget {
   @override
@@ -9,54 +10,92 @@ class ForgotPassword extends StatefulWidget {
 class _ForgotPasswordState extends State<ForgotPassword> {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      width: ScreenUtil.getInstance().setWidth(200),
-      //height: ScreenUtil.getInstance().setHeight(200),
-      padding: EdgeInsets.only(bottom: 1),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8.0),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0.0, 15.0),
-                blurRadius: 15.0),
-            BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0.0, -10.0),
-                blurRadius: 10.0),
-          ]),
-      child: Padding(
-        padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Username",
-                style: TextStyle(
-                    fontFamily: "Poppins-Medium",
-                    fontSize: ScreenUtil.getInstance().setSp(35))),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "enter username",
-                  hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0)),
+    return Material(
+      child: SafeArea(
+        child: Container(
+          width: ScreenUtil.getInstance().setWidth(200),
+          height: ScreenUtil.getInstance().setHeight(200),
+          padding: EdgeInsets.only(top: 50),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0.0, 15.0),
+                    blurRadius: 15.0),
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0.0, -10.0),
+                    blurRadius: 10.0),
+              ]),
+          child: Padding(
+            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Username",
+                    style: TextStyle(
+                        fontFamily: "Poppins-Medium",
+                        fontSize: ScreenUtil.getInstance().setSp(45))),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "enter username",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0)),
+                ),
+                SizedBox(
+                  height: ScreenUtil.getInstance().setHeight(30),
+                ),
+                Text("Password",
+                    style: TextStyle(
+                        fontFamily: "Poppins-Medium",
+                        fontSize: ScreenUtil.getInstance().setSp(45))),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "enter password",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0)),
+                ),
+                SizedBox(
+                  height: ScreenUtil.getInstance().setHeight(35),
+                ),
+                Center(
+                  child: InkWell(
+                    child: Container(
+                      width: ScreenUtil.getInstance().setWidth(330),
+                      height: ScreenUtil.getInstance().setHeight(100),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Color(0xFF17ead9), Color(0xFF6078ea)]),
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xFF6078ea).withOpacity(.3),
+                                offset: Offset(0.0, 8.0),
+                                blurRadius: 8.0)
+                          ]),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Center(
+                            child: Text(
+                              "SIGN IN",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "Poppins-Bold",
+                                  fontSize: 18,
+                                  letterSpacing: 1.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-            SizedBox(
-              height: ScreenUtil.getInstance().setHeight(30),
-            ),
-            Text("Password",
-                style: TextStyle(
-                    fontFamily: "Poppins-Medium",
-                    fontSize: ScreenUtil.getInstance().setSp(35))),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: "enter password",
-                  hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0)),
-            ),
-            SizedBox(
-              height: ScreenUtil.getInstance().setHeight(35),
-            ),
-          ],
+          ),
         ),
       ),
     );
